a130963e1027bde70ba0d7f6a14ee6869c256294
Philippe DAMOUNE <pdamoune@e2r8p11.42.fr>
2017-03-23 07:41:50 +0100

    memmove et memrealloc

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 15:40:40 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/03/21 21:55:00 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/03/23 02:24:44 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,12 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t			i;
-	unsigned char	*tmp;
 
 	i = 0;
-	if ((tmp = (unsigned char *)ft_memalloc(sizeof(unsigned char) * n)))
-	{
-		while (i < n)
-		{
-			tmp[i] = ((unsigned char *)src)[i];
-			i++;
-		}
-		ft_memcpy(dest, tmp, n);
-		free(tmp);
-		tmp = NULL;
-		return (dest);
-	}
-	return ((void *)src);
+	if (src > dest)
+		ft_memcpy(dest, src, n);
+	else
+		while (n--)
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	return (dest);
 }
