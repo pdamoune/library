f6d573cd05317cbc49d10f2d84ab9784a6e05cec
Philippe DAMOUNE <pdamoune@e2r8p11.42.fr>
2017-03-29 20:01:54 +0200

    nbrbin

--- src/ft_putnbr_bin.c
+++ src/ft_putnbr_bin.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/14 07:14:41 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/03/14 07:42:00 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/03/29 09:35:47 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ void	ft_putnbr_bin(int n)
 
 	i = 32;
 	while (--i >= 0)
+	{
 		(1 << i) & n ? ft_putchar ('1') : ft_putchar('0');
+		if (i % 8 == 0)
+			ft_putchar(' ');
+	}
 }
