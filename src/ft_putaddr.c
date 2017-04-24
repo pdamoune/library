70ebf8c479e093e5fad2ed541720037e1b5e0f05
philippe damoune <philippedamoune@gmail.com>
2017-04-24 04:35:43 +0200

    modif U L L

--- src/ft_putaddr.c
+++ src/ft_putaddr.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 05:07:44 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/04/15 21:40:58 by philippedamoune  ###   ########.fr       */
+/*   Updated: 2017/04/24 04:35:10 by philippedamoune  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 void	ft_putaddr(void *ptr)
 {
 	ft_putstr("0x");
-	ft_putstr(ft_ulltoa_base((U L L)ptr, 16));
+	ft_putstr(ft_ulltoa_base((unsigned long long)ptr, 16));
 	ft_putendl("");
 }
