0f011f66c15e341ceada60b4e3b6ccdcb0e1526c
Philippe DAMOUNE <pdamoune@e2r12p13.42.fr>
2017-08-11 16:14:49 +0200

    update color dans string

--- src/ft_printf/src/functions/prf_fill_data.c
+++ src/ft_printf/src/functions/prf_fill_data.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 16:24:04 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/04/26 19:30:32 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/08/11 12:02:15 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 void	prf_fill_data(char **data, int len)
 {
+	int	i;
+
 	if (!*data)
 		return ;
-	while (**data && len--)
+	while (**data && **data != '{' && len--)
 		prf_fill_c(*(*data)++);
+	if (**data == '{')
+	{
+		i = I;
+		I = 0;
+		prf_color(*data);
+		I = i;
+		*data += 5;
+		prf_fill_data(data, len - 4);
+	}
 	*data = NULL;
 }
